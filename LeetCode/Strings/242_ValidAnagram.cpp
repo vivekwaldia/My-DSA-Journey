cca3cdff2196aa510a6f388e114b3f9ee35e8b2b
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-25 02:06:06 +0530

    Add isAnagram function for Valid Anagram problem
    Implement the isAnagram function to check if two strings are anagrams by using a frequency array.

---
+++ LeetCode/Strings/242_ValidAnagram.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem: 242. Valid Anagram
+ * Platform: LeetCode
+ * Difficulty: Easy
+ * Logic: Map the frequencies by incrementing/decrementing the frequency array for each string respectively. Return false if all frequencies don't get cancelled out, otherwise return true.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq[26] = {0};
+
+        for(int i=0; i<s.length(); i++){
+            freq[s[i]-'a']++;
+        }
+
+        for(int i=0; i<t.length(); i++){
+            freq[t[i]-'a']--;
+        }
+
+        for(int i=0; i<26; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
