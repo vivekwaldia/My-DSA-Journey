2d813ac5093ebc350f1461084297c5a145e24e04
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-26 11:21:51 +0530

    Add solution for Deleting Array Again problem
    Implement solution for DELARR77 problem on CodeChef.

---
+++ CodeChef/Contests/Starters_218_Div4/D_Deleting_Array_Again.cpp
@@ -0,0 +1,54 @@
+/*
+ * Problem: DELARR77 - Deleting Array Again
+ * Contest: Starters 218
+ * Platform: CodeChef
+ * Rating: 1444
+ * Logic: Elements can only shift to left positions, calculate the minimum cost from C and multiply by current element of A and sum up to the total cost.
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve(){
+
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    
+    for(int i=0; i<n; i++) cin >> A[i];
+
+    vector<int> C(n);
+
+    for(int i=0; i<n; i++) cin >> C[i];
+
+    int totalCost = 0, currMinCost = C[0];
+
+    for(int i=0; i<n; i++){
+        if(C[i] < currMinCost){
+            currMinCost = C[i];
+        }
+
+        totalCost += (A[i]*currMinCost);
+    }
+
+    cout << totalCost << "\n";
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
