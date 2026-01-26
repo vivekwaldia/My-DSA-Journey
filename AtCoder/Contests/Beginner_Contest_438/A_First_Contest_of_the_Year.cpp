0ff556e16661bc20b9cba355babd167fc858ddea
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-26 15:11:09 +0530

    Create A_First_Contest_of_the_Year.cpp

---
+++ AtCoder/Contests/Beginner_Contest_438/A_First_Contest_of_the_Year.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem: A - First Contest of the Year
+ * Contest: AtCoder Beginner Contest 438
+ * Platform: AtCoder
+ * Points: 100
+ * Logic: Calculate remaining days and check for modular congruency with 7.
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve(){
+
+    int d, f;
+    cin >> d >> f;
+
+    int remDays = d % 7;
+
+    int ans = f-remDays;
+
+    if(ans <= 0){
+        ans += 7;
+    }
+
+    cout << ans << "\n";
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
