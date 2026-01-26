3153f96767bb692f9386d5314d3d2d760447c8cb
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-26 12:10:22 +0530

    Add solution for FBMATCH problem in C++
    Implement solution for Front Back Matching problem on CodeChef.

---
+++ CodeChef/Contests/Starters_220_Div4/B_Front_Back_Matching.cpp
@@ -0,0 +1,59 @@
+/*
+ * Problem: FBMATCH - Front Back Matching
+ * Contest: Starters 220
+ * Platform: CodeChef
+ * Rating: 632
+ * Logic: Map the frequencies and check if any element occurs more than once.
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve(){
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    bool isPossible = false;
+
+    int freq[26] = {0};
+
+    for(int i=0; i<n; i++){
+        int idx = s[i]-'a';
+
+        freq[idx]++;
+
+        if(freq[idx] > 1){
+            isPossible = true;
+            break;
+        }
+    }
+
+    if(isPossible){
+        cout << "Yes" << "\n";
+    }
+    else{
+        cout << "No" << "\n";
+    }
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
