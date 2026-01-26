b82009af8dd52a877b371d90d9e966a028c7da43
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-26 12:21:49 +0530

    Add solution for Protein Diet problem
    Implement a solution for the Protein Diet problem.

---
+++ CodeChef/Contests/Starters_221_Div4/A_Protein_Diet.cpp
@@ -0,0 +1,41 @@
+/*
+ * Problem: LMP1 - Protein Diet
+ * Contest: Starters 221
+ * Platform: CodeChef
+ * Rating: N/A
+ * Logic: Check if x >= y.
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve(){
+
+    int x, y;
+    cin >> x >> y;
+
+    if(x >= y){
+        cout << "Yes" << "\n";
+    }
+    else{
+        cout << "No" << "\n"; 
+    }
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
