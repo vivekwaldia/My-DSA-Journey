1d17a2d43034a099e25c6d9dbd2d2eed42de055e
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-26 09:27:24 +0530

    Add solution for Christmas Trees problem
    Implement solution for XMASTREE problem on CodeChef.

---
+++ CodeChef/Contests/Starters_218_Div4/XMASTREE_Christmas_Trees.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem: XMASTREE - Christmas Trees
+ * Contest: Starters 218
+ * Platform: CodeChef
+ * Rating: 300
+ * Logic: Subtract the total cost of sold trees with bought trees.
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve(){
+
+    int n, a, m, b;
+    cin >> n >> a >> m >> b;
+
+    cout << (m*b)-(n*a) << "\n";
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
