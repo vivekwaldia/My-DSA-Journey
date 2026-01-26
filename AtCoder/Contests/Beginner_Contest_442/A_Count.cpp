e823deef3a56a02c68afec4b6407ea09d916dc86
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-26 15:52:00 +0530

    Create A_Count.cpp

---
+++ AtCoder/Contests/Beginner_Contest_442/A_Count.cpp
@@ -0,0 +1,46 @@
+/*
+ * Problem: A - Count
+ * Contest: AtCoder Beginner Contest 442
+ * Platform: AtCoder
+ * Points: 100
+ * Logic: Iterate over the string to check for lower-case i and j(dots).
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve(){
+
+    string s;
+    cin >> s;
+
+    int ans = 0;
+
+    for(int i=0; i<s.length(); i++){
+        if(s[i] >= 'a' && s[i] <= 'z'){
+            if(s[i] == 'i' || s[i] == 'j'){
+                ans++;
+            }
+        }
+    }
+
+    cout << ans << "\n";
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
