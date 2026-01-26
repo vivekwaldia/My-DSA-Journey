6083345a45f2e71f607dca2351058bd937ae83da
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-26 14:18:15 +0530

    Add modular exponentiation function
    Implement modular exponentiation to calculate a^b % MOD efficiently.

---
+++ CSES/Mathematics/Exponentiation.cpp
@@ -0,0 +1,48 @@
+/*
+ * Problem: Exponentiation
+ * Platform: CSES
+ * Logic: Use modular exponentiation for fast calculation.
+ * Time Complexity: O(log N)
+ * Space Complexity: O(log N)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int MOD = 1e9+7;
+
+int binPow(int a, int b){
+
+    if(b == 0) return 1;
+
+    if((b & 1) == 1){
+        return (a * binPow(a, b-1)) % MOD;
+    }
+    
+    int temp = binPow(a, b/2);
+    return (temp * temp) % MOD;
+}
+
+void solve(){
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << binPow(a, b) << "\n";
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
