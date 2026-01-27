e798996eb397a588a22440e00c953250f664c7cf
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-27 19:42:34 +0530

    Add solution for LeetCode problem 3818

---
+++ LeetCode/!Contests/Weekly_Contest_486/3818_Minimum_Prefix_Removal.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem: 3818. Minimum Prefix Removal to Make Array Strictly Increasing
+ * Platform: LeetCode
+ * Difficulty: Medium
+ * Logic: Iterate backwards from the end of the array to identify the longest suffix that is strictly increasing.
+ - Stop at the first index where the strict increasing order is violated (current element <= previous element).
+ - The index at which the loop breaks represents the minimum number of elements (prefix length) that must be removed.
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-1;
+
+        for(; i>0; i--){
+            if(nums[i] <= nums[i-1]){
+                break;
+            }
+        }
+
+        return i;
+    }
+};
