b3177916f384e01a91cb9756a4aac3c214f0c47d
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-27 19:27:43 +0530

    Add solution for LeetCode problem 3809

---
+++ LeetCode/!Contests/Biweekly_Contest_174/3809_Best_Reachable_Tower.cpp
@@ -0,0 +1,59 @@
+/*
+ * Problem: 3809. Best reachable tower
+ * Platform: LeetCode
+ * Difficulty: Medium
+ * Logic: Iterate through each tower to calculate its Manhattan distance from the given center.
+ - Check if the tower is within the radius; if so, consider it as a candidate.
+ - Update the best tower if the current one has higher quality, or equal quality with lexicographically smaller coordinates.
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> bestTower(vector<vector<int>>& towers, vector<int>& center, int radius) {
+        vector<int> ans(2, -1);
+        int highestQuality = INT_MIN;
+
+        int cx = center[0];
+        int cy = center[1];
+
+        int n = towers.size();
+
+        for(int i=0; i<n; i++){
+            int xc = towers[i][0];
+            int yc = towers[i][1];
+            int qf = towers[i][2];
+
+            int fp = abs(xc-cx);
+            int sp = abs(yc-cy);
+            int md = fp+sp;
+
+            if(md > radius){
+                continue;
+            }
+            else{
+                if(qf > highestQuality){
+                    ans = {xc, yc};
+                    highestQuality = qf;
+                }
+                else if(qf == highestQuality){
+                    if(xc < ans[0]){
+                        ans = {xc, yc};
+                    }
+                    else if(xc == ans[0]){
+                        if(yc < ans[1]){
+                            ans = {xc, yc};
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
