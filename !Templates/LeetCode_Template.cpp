f285fe306fac445bfdd6f5da786b8da69fc274c7
Vivek Waldia <vivekwaldia71@gmail.com>
2026-01-29 00:47:28 +0530

    Add LeetCode template for better performance

---
+++ !Templates/LeetCode_Template.cpp
@@ -0,0 +1,11 @@
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    // Paste function signature here
+};
